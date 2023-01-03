6688fc2f682c6e15705da1d4ce8edc6d73221163
Omkar Kesarkhane <omkesarkhane@gmail.com>
2023-01-04 00:24:51 +0530

    ADD : added ids

--- src/components/app-drawer/AppDrawer.js
+++ src/components/app-drawer/AppDrawer.js
@@ -14,11 +14,11 @@ const AppDrawer = (props) => {
     }, [props.width, props.direction])
 
     return(
-        <div className="app-drawer-container">
-           <div className="drawer" style={getDrawerStyle}>
-                <div className="drawer-header px-3 py-2">
-                    <p className="header-title mb-0 pr-2">{props['header']}</p>
-                    <img className="close-icon" alt="close" src={cross} onClick={props['close']}/>
+        <div id="app-drawer" className="app-drawer-container">
+            <div className="drawer" style={getDrawerStyle}>
+                <div id="app-drawer-header-block" className="drawer-header px-3 py-2">
+                    <p id="app-drawer-header-title" className="header-title mb-0 pr-2">{props['header']}</p>
+                    <img id="app-drawer-header-close-button" className="close-icon" alt="close" src={cross} onClick={props['close']}/>
                 </div>
                 <div className="drawer-content px-3 py-2">
                     {props.children}
