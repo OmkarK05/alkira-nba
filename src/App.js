d0caaf53717273628dfa934a412a68d19639c42a
Omkar Kesarkhane <omkesarkhane@gmail.com>
2023-01-04 12:37:04 +0530

    CORE: added bs theme config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { ThemeProvider } from 'react-bootstrap';
-import './App.css';
 import NbaTeams from './pages/nba-teams/NbaTeams';
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider
-        breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-        minBreakpoint="xxs"
-      >
         <NbaTeams />
-      </ThemeProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import './services/axiosInterpreter'
+import './services/axiosInterpreter';
+import './App.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
